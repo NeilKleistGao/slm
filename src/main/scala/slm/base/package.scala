b3fa3d7d0649e367c79f6e1af916b84ec6781ac8
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-06-11 11:36:39 +0800

    Use package object instead

--- src/main/scala/slm/base/package.scala
+++ src/main/scala/slm/base/package.scala
@@ -1,8 +1,6 @@
 package slm
 
-import slm.base._;
-
-object SLM {
+package object base {
   def add[Expr](lhs: Expr, rhs: Expr)(implicit env: SemiRing[Expr]): Expr = env.add(lhs, rhs)
   def mul[Expr](lhs: Expr, rhs: Expr)(implicit env: SemiRing[Expr]): Expr = env.mul(lhs, rhs)
   def one[Expr](implicit env: SemiRing[Expr]): Expr = env.one
@@ -12,4 +10,6 @@ object SLM {
   def sub[Expr](lhs: Expr, rhs: Expr)(implicit env: Ring[Expr]): Expr = env.sub(lhs, rhs)
 
   def dim[VectorExpr, DimExpr](vec: VectorExpr)(implicit env: Module[VectorExpr, _, DimExpr]): DimExpr = env.dim(vec)
+  def add[VectorExpr](lhs: VectorExpr, rhs: VectorExpr)(implicit env: Module[VectorExpr, _, _]): VectorExpr = env.add(lhs, rhs)
+  def smul[ScaleExpr, VectorExpr](s: ScaleExpr, v: VectorExpr)(implicit env: Module[VectorExpr, ScaleExpr, _]): VectorExpr = env.smul(s, v)
 }
